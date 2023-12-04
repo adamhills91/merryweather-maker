2cba33cfa08722270041907746e5344bb49a3501
adamhills-hf <adam@hamblyfreeman.com>
2023-12-04 22:22:27 +0000

    Added policy page template

---
+++ policy-page-template.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: Policy Page */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+  <?php the_title();
+
+  the_content();
+
+  ?>
+
+</main><!-- #main -->
+
+<?php
+// get_sidebar();
+get_footer();
